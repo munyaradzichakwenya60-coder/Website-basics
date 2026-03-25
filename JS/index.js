dd49bf360f66e92f9cced8637aa3ec739eeec282
munyaradzichakwenya60-coder <munyaradzichakwenya60@gmail.com>
2026-03-25 08:50:39 +0200

    first commit

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+let name="rich";
+let age=30;
+let isStudent=false;
+console.log("Name:"+ name);
+console.log("Age:"+age);
+console.log("is Student:"+ isStudent)
+let x=10;
+let y=5;
+let sum= x+y;
+let product=x*y;
+let divide=x/y;
+let minus=x-y;
+console.log(x+y);
+console.log(x*y);
+console.log(x/y);
+console.log(x-y);
+console.log("10"+5);
+parseInt("5")
+let result=(10+5)*2;
+console.log(result);
+
